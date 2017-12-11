55403b35b67e8c2e8d597c9bbd6a31f8cf4fefbc
SamoxPro <samuel.heredia@smartclarity.com>
2017-12-11 17:21:32 -0300

    Cambios Holder

--- app/src/main/java/com/example/katiuska/listview/MyAdapter.kt
+++ app/src/main/java/com/example/katiuska/listview/MyAdapter.kt
@@ -1,27 +1,42 @@
 package com.example.katiuska.listview
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.CheckBox
+import android.view.LayoutInflater
+
+
+
+
 
 /**
  * Created by katiuska on 12/6/17.
  */
 
 class MyAdapter(val items: List<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>(){
+    var context =null as Context?
 
-
+    @Override
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
+        if(items[position] == "banana") {
+            holder.view.botto
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(TextView(parent.context))
+        this.context = parent.context
+        return ViewHolder(View(parent.context))
     }
 
     override fun getItemCount(): Int =  items.size
 
 
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
 
 }
+
+
